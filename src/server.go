a11ec735c08581fb7f93a9013e6f6712527434d5
Ryan Smith <ryan.l.smith142@gmail.com>
2021-02-11 21:32:41 -0600

    Starting to create connection to postgres

--- src/server.go
+++ src/server.go
@@ -1,10 +1,27 @@
 package main
 
-import ( 
+import (
+    "database/sql" 
     "fmt"
     "net/http"
+
+    _"github.com/lib/pq"
 )
 
+const (
+    host = "localhost"
+    port = 5432
+    user = "postgres"
+    password = "password"
+    dbname = ""
+)
+
+db, err := sql.Open("postgres", psqlInfo)
+if err != nil {
+  panic(err)
+}
+
+defer db.Close()
 func helloWorld(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Hello World")
 }
@@ -13,3 +30,4 @@ func main() {
     http.HandleFunc("/", helloWorld)
     http.ListenAndServe(":8080", nil)
 }
+
