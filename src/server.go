6aed879830fc0c73ceb7f71001b7642db3e08a2c
Ryan Smith <ryan.l.smith142@gmail.com>
2021-02-13 19:07:10 -0600

    Adds rest of connection for postgres

--- src/server.go
+++ src/server.go
@@ -16,17 +16,30 @@ const (
     dbname = ""
 )
 
-db, err := sql.Open("postgres", psqlInfo)
-if err != nil {
-  panic(err)
-}
-
-defer db.Close()
 func helloWorld(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Hello World")
 }
 
 func main() {
+    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
+
+    db, err := sql.Open("postgres", psqlInfo)
+
+    if err != nil {
+    panic(err)
+    }
+
+    defer db.Close()
+
+    err = db.Ping()
+    if err != nil {
+    panic(err)
+    }
+
+    fmt.Println("Successfully connected!")
+    
     http.HandleFunc("/", helloWorld)
     http.ListenAndServe(":8080", nil)
 }
